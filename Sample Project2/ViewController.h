56db6debd3d8a51210fdcdfc4cea43f703619c74
vm mac <vmmac@vms-mac.local>
2016-06-16 09:56:34 +0530

    Initial Commit

---
+++ Sample Project2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sample Project2
+//
+//  Created by vm mac on 16/06/2016.
+//  Copyright © 2016 PytenLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Sample Project2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Sample Project2
+//
+//  Created by vm mac on 16/06/2016.
+//  Copyright © 2016 PytenLabs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
